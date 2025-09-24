77e45bf700726ba0a97722bf9f7182e9ccf43650
AbhirupRoy27 <abhiruproyofficial@gmail.com>
2025-09-24 21:00:52 +0530

    feat(users): implement basic CRUD endpoints with MongoDB integration and duplicate handling

--- Basic_CRUD/server.js
+++ Basic_CRUD/server.js
@@ -17,4 +17,40 @@ async function connectDB() {
 }
 connectDB()
 
+app.post('/users', async (req, res) => {
+  const { name, email, job } = req.body
+
+  const newUser = new Users({
+    name: name, // String
+    email: email, // String
+    job: job, // String
+  })
+
+  // instance object of the model.
+  try {
+    await newUser.save()
+    console.log('User saved!')
+    res.json({ message: 'API Updated' })
+  } catch (err) {
+    if (err.name === 'MongoServerError')
+      switch (err.code) {
+        case 11000:
+          res.send('duplicate Data')
+          break
+        default:
+          console.log('Other MongoServerError')
+      }
+  }
+})
+
+app.get('/', async (req, res) => {
+  try {
+    const users = await Users.find({})
+    res.status(200).json(users)
+  } catch (err) {
+    console.error(err)
+    res.status(500).send('Server error')
+  }
+})
+
 app.listen(3000, () => console.log(`server at PORT: ${3000}`))
